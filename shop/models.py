15aae976b346046b1aa90850ba9ffc6a8efb49be
vkhankina <vkhankina@gmail.com>
2021-04-21 14:23:52 +0300

    db connection setup, models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Cart(models.Model):
+    class CartState(models.TextChoices):
+        NEW = 'new', _('new')
+        EXPIRED = 'expired', _('expired')
+        SOLD = 'sold', _('sold')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    state = models.CharField(choices=CartState.choices, default=CartState.NEW, max_length=7)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+    qty = models.IntegerField(default=1, validators=[MinValueValidator(0)])
--- trial/settings.py
+++ trial/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'trial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trial_shop_dev',
+        'USER': 'trial_shop_dev_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
